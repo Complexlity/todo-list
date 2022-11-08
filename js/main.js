4985184e5ee9fbedafb4d9549e9e66b297093140
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-09 00:23:10 +0100

    Add populate content for the mobile menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 const hamburgerIcon = document.querySelector('.hamburger')
-const secondMenu = document.querySelector('.second-menu')
 const hamburgerCloseIcon = document.querySelector('.hamburger-close')
+const secondMenu = document.querySelector('.mobile-menu')
+const primaryMenu = document.querySelector('.primary-menu')
+const secondMenuContent = document.querySelector('.mobile-menu-content')
 
-console.log(hamburgerCloseIcon)
+
+console.log(primaryMenu, secondMenuContent)
 hamburgerIcon.addEventListener('click', displaySecMenu)
 hamburgerCloseIcon.addEventListener('click', hideSecMenu)
 
 function displaySecMenu(){
+    populateMenu()
     secondMenu.style.transform = 'translateX(0)'
     this.style.display = 'none'
+    
 }
 
 function hideSecMenu(){
     secondMenu.style.transform = 'translateX(100%)'
     setTimeout(() => hamburgerIcon.style.display = 'block', 900)
+}
+
+function populateMenu(){
+    secondMenuContent.innerHTML = primaryMenu.innerHTML
 }
