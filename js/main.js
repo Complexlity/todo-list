9997f73f527d17550c7789ddcc87551bb60b046c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-09 20:21:42 +0100

    Create main content layout

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ const hamburgerCloseIcon = document.querySelector('.hamburger-close')
 const secondMenu = document.querySelector('.mobile-menu')
 const primaryMenu = document.querySelector('.primary-menu')
 const secondMenuContent = document.querySelector('.mobile-menu-content')
+const todoItems = document.querySelector('.todo-items')
 
-
-console.log(primaryMenu, secondMenuContent)
 hamburgerIcon.addEventListener('click', displaySecMenu)
 hamburgerCloseIcon.addEventListener('click', hideSecMenu)
+populateTodoItems(5)
+
 
 function displaySecMenu(){
     populateMenu()
@@ -18,9 +19,25 @@ function displaySecMenu(){
 
 function hideSecMenu(){
     secondMenu.style.transform = 'translateX(100%)'
-    setTimeout(() => hamburgerIcon.style.display = 'block', 900)
+    setTimeout(() => hamburgerIcon.style.display = 'block', 600)
 }
 
 function populateMenu(){
     secondMenuContent.innerHTML = primaryMenu.innerHTML
+}
+
+function populateTodoItems(number){
+    let item = `<li class="todo w-full  bg-red-600 rounded-lg px-4 py-2">
+    <div class="todo-text-container">
+    <input type='checkbox' class="check-todo-btn"></input>
+    <input class="w-4/5"type="text" class="todo-text" value="Go to the gym at least once every day" readonly disabled>
+    <div class="btn-container">
+    <i class="delete-todo-btn far fa-trash"></i>
+    <i class="edit-todo-btn fas fa-edit"></i>
+    </div></div>
+    </li>
+    `
+    for (let i = 0; i < number; i++){
+        todoItems.innerHTML += item
+    }
 }
